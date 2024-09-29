b34063ce0de66ab00a1db24b273e9590b8dd7718
Ayushi-Rahane <ayushirahane2021@gmail.com>
2024-09-29 19:06:29 +0530

    c

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -3,7 +3,10 @@ import React, {useState} from 'react'
 export default function TextForm({heading}) {
     const [text, setText] = useState("Enter text here:");
    // setText("Text2");
-
+   
+   const handleOnFocus =()=> {
+       setText("");
+   }
    const handleOnChange = (event)=>{
       setText(event.target.value);
    }
@@ -17,25 +20,34 @@ export default function TextForm({heading}) {
     let newText = text.toLowerCase();
     setText(newText);
    }
+
+   const handleSentenceClick = ()=>{
+ 
+    setText(   
+      text.split(" ").map(x =>{
+        return x.charAt(0).toUpperCase() + x.slice(1).toLowerCase();
+        
+      }).join(" "));
+   }
   return (
   <>
     <div>
      
      <div className="mb-3">
          <h1>{heading}</h1>
-         <textarea  className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+         <textarea  className="form-control" id="myBox" rows="8" value={text} onFocus={handleOnFocus} onChange={handleOnChange}></textarea>
      </div>
 
      <button className="btn btn-primary mx-2" onClick={handleUpperClick}>Convert to Uppercase</button>
      <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Lowercase</button>
-      <button className="btn btn-primary mx-2" onClick={handleLowerClick}>Convert to Sentencecase</button>
+      <button className="btn btn-primary mx-2" onClick={handleSentenceClick}>Convert to Sentencecase</button>
 
 
 </div>
 <div className="container my3">
 <h2>Your text summary</h2>
-<p>{text.split(" ").length} words and {text.length} characters</p>
-<p> {0.008*text.split(" ").length} Minutes read</p>
+<p>{text.split(" ").filter(x => {if(x!==' '){return x} else{ return false;}}).length} words and {text.split("").filter(x => {if(x!==' '){return x} else{ return false;}}).length} characters</p>
+<p> {0.008*text.split(" ").filter(x => {if(x!==' '){return x} else{ return false;} }).length} Minutes read</p>
 <h3>Preview</h3>
 <p>{text}</p>
 </div>
